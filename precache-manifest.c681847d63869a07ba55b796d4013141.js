6875ae0f6cd88dacd948c9e2f8f5af22afb6d51e
Anna Gerdii <annagerdiy@gmail.com>
2019-12-10 18:48:03 +0200

    Updates

---
+++ precache-manifest.c681847d63869a07ba55b796d4013141.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a58918e66b805d4f9b61d39caae6596",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "8a9b59e978fa62e40eb7",
+    "url": "/react_add-todo-form/static/css/main.0a24385e.chunk.css"
+  },
+  {
+    "revision": "68b1658d02f8c0e09e9f",
+    "url": "/react_add-todo-form/static/js/2.1c2721b6.chunk.js"
+  },
+  {
+    "revision": "8a9b59e978fa62e40eb7",
+    "url": "/react_add-todo-form/static/js/main.8c20c8e0.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
